6daf86c7b6c34eaa09d18992ca332003e468057c
moloy559 <43271232+moloy559@users.noreply.github.com>
2018-09-21 10:36:37 -0400

    added files via upload

---
+++ starter-template/hello.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>My First PHP </title>
+</head>
+<body>
+<h1>My First PHP page</h1>
+<?php echo "<strong>Hello world!</strong>"; ?>
+<br>
+<?php echo "PHP is \"Fun\"" ?>
+<br>
+<?php echo "&copy; " . date("Y") . " Rob McCormack"; ?>
+</body>
+</html>
