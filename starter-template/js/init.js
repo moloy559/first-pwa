1ba4d72e8d03e8105f6fe3a7b3b43ef4bdfe0f18
imedia <imedia@imd-im-27a.camp.confederationc.on.ca>
2018-10-05 10:27:22 -0400

    new commit

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -3,5 +3,10 @@
 
     $('.sidenav').sidenav();
 
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems, options);
+    });
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
